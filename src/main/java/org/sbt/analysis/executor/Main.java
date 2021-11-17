0ec304d4628a956ef39a72f44b4cb716e56430c8
mdvorchik <maxim.dvorchik@yandex.ru>
2021-11-17 14:39:20 +0300

    init commit

---
+++ src/main/java/org/sbt/analysis/executor/Main.java
@@ -0,0 +1,7 @@
+package org.sbt.analysis.executor;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Ready to analysis");
+    }
+}
---
+++ src/test/java/org/sbt/analysis/executor/MainTest.java
@@ -0,0 +1,8 @@
+package org.sbt.analysis.executor;
+
+import org.junit.Test;
+
+public class MainTest {
+    @Test
+    public void empty(){}
+}
